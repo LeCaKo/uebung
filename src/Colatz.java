4bf5d21ded06592041a6d9fe305c9b6f1130dd41
Koschel.Leonard <Koschel.Leonard@SK-D43D7E37FA4C.2631.skool.net>
2018-09-20 08:29:30 +0200

    Colatz bis 100

--- src/Colatz.java
+++ src/Colatz.java
@@ -3,8 +3,15 @@ public class Colatz {
 
 	public static void main(String[] args) {
 		
-		int zahl = 42;
-				
+		
+		int zahl;
+		
+		for(int i = 1; i<=100;i++ ){
+		
+		zahl = i ;
+		
+		System.out.print("Seed:" + i+"| ");
+		
 		while (zahl >= 2){
 		
 		if(zahl % 2 != 0) {
@@ -15,8 +22,11 @@ public class Colatz {
 		}
 		System.out.print(zahl + ", ");
 		
+		
 		}
+		System.out.println("");
 		
 	}
 
+}
 }
