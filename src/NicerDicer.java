a77dc0885bb561076813a0c9e64abf4d8585958e
Koschel.Leonard <Koschel.Leonard@DESKTOP-FQL6HA1.2631.skool.net>
2018-10-04 09:02:23 +0200

    Glücksspiel

---
+++ src/NicerDicer.java
@@ -0,0 +1,32 @@
+
+public class NicerDicer {
+
+	public static void main(String[] args) {
+
+		int alter = 17 ;
+		
+		if(alter>=18){
+			
+			if(Math.random()*100-1>=42){
+				
+				System.out.println( "Schade!  Leider haben Sie verloren. Versuchen Sie ihr Glück odch nocheinmal!" );
+				
+			}else{		
+			
+				System.out.println( "Herzlichen Glückwunsch! Sie haben gewonnen!" );
+				
+			}
+			
+			}else{
+				
+				System.out.println( "Du bist minderjährig und darfst noch kein Glücksspiel spielen!" );
+				
+			}
+			
+		}
+		
+		boolean geschlecht = true ;
+		boolean name = true ;
+		
+	}
+
